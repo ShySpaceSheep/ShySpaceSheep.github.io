9aec8fe49b41ebc8e2c3a999a05904815c48ce50
ShySpaceSheep <theminecraftf303@gmail.com>
2023-01-12 15:10:25 +0800

    Updated files

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
+window.onscroll = function() {scrollFunction()};
 
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementsByClassName("navigation-bar").style.padding = "30px 10px";
+    document.getElementById("website-logo").style.fontSize = "25px";
+  } else {
+    document.getElementsByClassName("navigation-bar").style.padding = "80px 10px";
+    document.getElementById("website-logo").style.fontSize = "35px";
+  }
+}
